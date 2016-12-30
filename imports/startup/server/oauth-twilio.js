fb7b41d53c4feef55192b0004e5c5d32e7a294fd
balibou <benjamin.cherion@gmail.com>
2016-12-30 21:16:07 +0000

    adding custom account phone package

--- imports/startup/server/oauth-twilio.js
+++ imports/startup/server/oauth-twilio.js
@@ -1,9 +1,9 @@
 const init = () => {
-  SMS.twilio = {
-    FROM: Meteor.settings.oauth.twilio.phone,
-    ACCOUNT_SID: Meteor.settings.oauth.twilio.accountSid,
-    AUTH_TOKEN: Meteor.settings.oauth.twilio.authToken,
-  };
+  // SMS.twilio = {
+  //   FROM: Meteor.settings.oauth.twilio.phone,
+  //   ACCOUNT_SID: Meteor.settings.oauth.twilio.accountSid,
+  //   AUTH_TOKEN: Meteor.settings.oauth.twilio.authToken,
+  // };
 }
 
 export default init;
